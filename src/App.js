fff7d120efd66288af64266c34429d09645f5137
router-resources <124175970+router-resources@users.noreply.github.com>
2024-06-23 18:35:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,914 @@
+import React,{useState} from "react"
+import logo from './logo.svg';
+import './App.css';
+import OpenAI from "openai";
+import axios from "axios"
+import { ethers } from 'ethers';
+
+
+
+function App() {
+
+  const [quoteData,setQuoteData]=useState()
+  const [account,setAccount]=useState("Connect Wallet")
+
+  const erc20_abi = [
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "spender",
+					"type": "address"
+				},
+				{
+					"internalType": "uint256",
+					"name": "value",
+					"type": "uint256"
+				}
+			],
+			"name": "approve",
+			"outputs": [
+				{
+					"internalType": "bool",
+					"name": "",
+					"type": "bool"
+				}
+			],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "uint256",
+					"name": "value",
+					"type": "uint256"
+				}
+			],
+			"name": "burn",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "account",
+					"type": "address"
+				},
+				{
+					"internalType": "uint256",
+					"name": "value",
+					"type": "uint256"
+				}
+			],
+			"name": "burnFrom",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "initialOwner",
+					"type": "address"
+				}
+			],
+			"stateMutability": "nonpayable",
+			"type": "constructor"
+		},
+		{
+			"inputs": [],
+			"name": "ECDSAInvalidSignature",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "uint256",
+					"name": "length",
+					"type": "uint256"
+				}
+			],
+			"name": "ECDSAInvalidSignatureLength",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "bytes32",
+					"name": "s",
+					"type": "bytes32"
+				}
+			],
+			"name": "ECDSAInvalidSignatureS",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "spender",
+					"type": "address"
+				},
+				{
+					"internalType": "uint256",
+					"name": "allowance",
+					"type": "uint256"
+				},
+				{
+					"internalType": "uint256",
+					"name": "needed",
+					"type": "uint256"
+				}
+			],
+			"name": "ERC20InsufficientAllowance",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "sender",
+					"type": "address"
+				},
+				{
+					"internalType": "uint256",
+					"name": "balance",
+					"type": "uint256"
+				},
+				{
+					"internalType": "uint256",
+					"name": "needed",
+					"type": "uint256"
+				}
+			],
+			"name": "ERC20InsufficientBalance",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "approver",
+					"type": "address"
+				}
+			],
+			"name": "ERC20InvalidApprover",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "receiver",
+					"type": "address"
+				}
+			],
+			"name": "ERC20InvalidReceiver",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "sender",
+					"type": "address"
+				}
+			],
+			"name": "ERC20InvalidSender",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "spender",
+					"type": "address"
+				}
+			],
+			"name": "ERC20InvalidSpender",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "uint256",
+					"name": "deadline",
+					"type": "uint256"
+				}
+			],
+			"name": "ERC2612ExpiredSignature",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "signer",
+					"type": "address"
+				},
+				{
+					"internalType": "address",
+					"name": "owner",
+					"type": "address"
+				}
+			],
+			"name": "ERC2612InvalidSigner",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "account",
+					"type": "address"
+				},
+				{
+					"internalType": "uint256",
+					"name": "currentNonce",
+					"type": "uint256"
+				}
+			],
+			"name": "InvalidAccountNonce",
+			"type": "error"
+		},
+		{
+			"inputs": [],
+			"name": "InvalidShortString",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "to",
+					"type": "address"
+				},
+				{
+					"internalType": "uint256",
+					"name": "amount",
+					"type": "uint256"
+				}
+			],
+			"name": "mint",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "owner",
+					"type": "address"
+				}
+			],
+			"name": "OwnableInvalidOwner",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "account",
+					"type": "address"
+				}
+			],
+			"name": "OwnableUnauthorizedAccount",
+			"type": "error"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "string",
+					"name": "str",
+					"type": "string"
+				}
+			],
+			"name": "StringTooLong",
+			"type": "error"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": true,
+					"internalType": "address",
+					"name": "owner",
+					"type": "address"
+				},
+				{
+					"indexed": true,
+					"internalType": "address",
+					"name": "spender",
+					"type": "address"
+				},
+				{
+					"indexed": false,
+					"internalType": "uint256",
+					"name": "value",
+					"type": "uint256"
+				}
+			],
+			"name": "Approval",
+			"type": "event"
+		},
+		{
+			"anonymous": false,
+			"inputs": [],
+			"name": "EIP712DomainChanged",
+			"type": "event"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": true,
+					"internalType": "address",
+					"name": "previousOwner",
+					"type": "address"
+				},
+				{
+					"indexed": true,
+					"internalType": "address",
+					"name": "newOwner",
+					"type": "address"
+				}
+			],
+			"name": "OwnershipTransferred",
+			"type": "event"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "owner",
+					"type": "address"
+				},
+				{
+					"internalType": "address",
+					"name": "spender",
+					"type": "address"
+				},
+				{
+					"internalType": "uint256",
+					"name": "value",
+					"type": "uint256"
+				},
+				{
+					"internalType": "uint256",
+					"name": "deadline",
+					"type": "uint256"
+				},
+				{
+					"internalType": "uint8",
+					"name": "v",
+					"type": "uint8"
+				},
+				{
+					"internalType": "bytes32",
+					"name": "r",
+					"type": "bytes32"
+				},
+				{
+					"internalType": "bytes32",
+					"name": "s",
+					"type": "bytes32"
+				}
+			],
+			"name": "permit",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "renounceOwnership",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "to",
+					"type": "address"
+				},
+				{
+					"internalType": "uint256",
+					"name": "value",
+					"type": "uint256"
+				}
+			],
+			"name": "transfer",
+			"outputs": [
+				{
+					"internalType": "bool",
+					"name": "",
+					"type": "bool"
+				}
+			],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": true,
+					"internalType": "address",
+					"name": "from",
+					"type": "address"
+				},
+				{
+					"indexed": true,
+					"internalType": "address",
+					"name": "to",
+					"type": "address"
+				},
+				{
+					"indexed": false,
+					"internalType": "uint256",
+					"name": "value",
+					"type": "uint256"
+				}
+			],
+			"name": "Transfer",
+			"type": "event"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "from",
+					"type": "address"
+				},
+				{
+					"internalType": "address",
+					"name": "to",
+					"type": "address"
+				},
+				{
+					"internalType": "uint256",
+					"name": "value",
+					"type": "uint256"
+				}
+			],
+			"name": "transferFrom",
+			"outputs": [
+				{
+					"internalType": "bool",
+					"name": "",
+					"type": "bool"
+				}
+			],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "newOwner",
+					"type": "address"
+				}
+			],
+			"name": "transferOwnership",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "owner",
+					"type": "address"
+				},
+				{
+					"internalType": "address",
+					"name": "spender",
+					"type": "address"
+				}
+			],
+			"name": "allowance",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "account",
+					"type": "address"
+				}
+			],
+			"name": "balanceOf",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "decimals",
+			"outputs": [
+				{
+					"internalType": "uint8",
+					"name": "",
+					"type": "uint8"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "DOMAIN_SEPARATOR",
+			"outputs": [
+				{
+					"internalType": "bytes32",
+					"name": "",
+					"type": "bytes32"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "eip712Domain",
+			"outputs": [
+				{
+					"internalType": "bytes1",
+					"name": "fields",
+					"type": "bytes1"
+				},
+				{
+					"internalType": "string",
+					"name": "name",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "version",
+					"type": "string"
+				},
+				{
+					"internalType": "uint256",
+					"name": "chainId",
+					"type": "uint256"
+				},
+				{
+					"internalType": "address",
+					"name": "verifyingContract",
+					"type": "address"
+				},
+				{
+					"internalType": "bytes32",
+					"name": "salt",
+					"type": "bytes32"
+				},
+				{
+					"internalType": "uint256[]",
+					"name": "extensions",
+					"type": "uint256[]"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "name",
+			"outputs": [
+				{
+					"internalType": "string",
+					"name": "",
+					"type": "string"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "owner",
+					"type": "address"
+				}
+			],
+			"name": "nonces",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "owner",
+			"outputs": [
+				{
+					"internalType": "address",
+					"name": "",
+					"type": "address"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "symbol",
+			"outputs": [
+				{
+					"internalType": "string",
+					"name": "",
+					"type": "string"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "totalSupply",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		}
+	];
+	const PATH_FINDER_API_URL = "https://api.pf.testnet.routerprotocol.com/api"
+
+	const getQuote = async (params) => {
+		const endpoint = "v2/quote"
+		const quoteUrl = `${PATH_FINDER_API_URL}/${endpoint}`
+	
+		console.log(quoteUrl)
+	
+		try {
+			const res = await axios.get(quoteUrl, { params })
+      setQuoteData(res.data)
+			return res.data;
+		} catch (e) {
+			console.error(`Fetching quote data from pathfinder: ${e}`)
+		}    
+	}
+
+	const checkAndSetAllowance = async (wallet, tokenAddress, approvalAddress, amount) => {
+		
+		if (tokenAddress === ethers.constants.AddressZero) {
+			return
+		}
+	
+		
+		const erc20 = new ethers.Contract(tokenAddress, erc20_abi, wallet);
+		const allowance = await erc20.allowance(await wallet.getAddress(), approvalAddress);
+		if (allowance.lt(amount)) {
+			const approveTx = await erc20.approve(approvalAddress, amount, {gasPrice: await wallet.provider.getGasPrice()});
+			try {
+				await approveTx.wait();
+				console.log(`Transaction mined succesfully: ${approveTx.hash}`)
+			}
+			catch (error) {
+				console.log(`Transaction failed with error: ${error}`)
+			}
+		}
+		else{
+
+			console.log("enough allowance")
+			alert("enough allowance")
+		}
+	}
+	
+	const getTransaction = async (params, quoteData) => {
+		const endpoint = "v2/transaction"
+		const txDataUrl = `${PATH_FINDER_API_URL}/${endpoint}`
+	
+		console.log(txDataUrl)
+	
+		try {
+			const res = await axios.post(txDataUrl, {
+				...quoteData,
+				slippageTolerance: 0.5,
+				senderAddress: account,
+				receiverAddress: account,
+			})
+			return res.data;
+		} catch (e) {
+			console.error(`Fetching tx data from pathfinder: ${e}`)
+		}    
+	}
+
+  const [sentence,setSentence]=useState('')
+
+  let chainID={
+
+    amoy:'80002',
+    fuji:'43113',
+    holsky:'17000'
+  
+  }
+  
+  let tokenAddress={
+  
+    aftt:{
+      amoy:'0xBAD6e1AbE5EbEae8a123ef14AcA7024D3F8c45fb',
+      fuji:'0x69dc97bb33e9030533ca2006ab4cef67f4db4125',
+      holsky:'0x5c2c6ab36a6e4e160fb9c529e164b7781f7d255f'
+    }
+  }
+  
+  
+  
+  const extractVariables=async (sentence) =>{
+  
+    const openai = new OpenAI({
+      apiKey: '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',
+      dangerouslyAllowBrowser: true 
+    });
+    
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: `Extract the sourceToken, sourceChain, desToken, desChain, and amount from the following sentence. Source Chains can be Holsky, Fuji and Amoy . Similary Destion Chains can be Holsky, Fuji and Amoy. Source and Destination Tokens can be AFTT,USDT, USDC 
+    "${sentence}"
+    Return the results in the following format: 
+    {
+      "sourceToken": "x",
+      "sourceChain": "a",
+      "desToken":"y",
+      "desChain": "b",
+      "amount": "z"
+    }` }],
+      model: "gpt-3.5-turbo",
+    });
+  
+    const result=completion.choices[0].message["content"];
+    console.log(result)
+  
+    const variables = JSON.parse(result);
+  
+  
+   return variables;
+  
+  }
+
+  return (
+    <div className="App">
+
+<br></br>
+<button class="button-17" role="button" onClick={async ()=>{
+
+if(window.ethereum) {
+	console.log('detected');
+
+	try {
+	  const accounts = await window.ethereum.request({
+		method: "eth_requestAccounts",
+	  });
+
+	  setAccount(accounts[0])
+	
+	  console.log(accounts[0])
+	
+	 
+	}
+	catch(err) {
+	  console.log(err)
+	}
+  }
+
+		}}> {account!='Connect Wallet'?account.substring(0,4)+"...."+account.substring(38,42):account}</button>
+
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br> <br></br>
+    <br></br> <br></br>
+    <br></br> <br></br>
+  
+      <textarea cols="100" placeholder="Enter Prompt" onChange={(e)=>{
+        setSentence(e.target.value)
+      }}></textarea>
+   
+
+ <br></br>
+ <br></br> <br></br> <br></br> <br></br> <br></br>
+
+      <button  role="button" style={{width:'10em',height:'3em',backgroundColor:'white',borderRadius:'2em',borderColor:'black'}} onClick={async ()=>{
+
+        //Extracting Variables
+        const variables=await extractVariables(sentence)
+          if (variables) {
+           
+            const { sourceToken, sourceChain, desToken,desChain, amount } = variables;
+            let resObj={sourceToken:tokenAddress[`${sourceToken.toLowerCase()}`][`${sourceChain.toLowerCase()}`],sourceChain:chainID[`${sourceChain.toLowerCase()}`],
+            desToken:tokenAddress[`${desToken.toLowerCase()}`][`${desChain.toLowerCase()}`],desChain:chainID[`${desChain.toLowerCase()}`],amount:amount}
+            console.log(resObj)
+
+            //Getting the Quote
+            const params ={
+              'fromTokenAddress': resObj.sourceToken,
+              'toTokenAddress': resObj.desToken,
+              'amount': parseFloat(amount)*Math.pow(10,18),
+              'fromTokenChainId': resObj.sourceChain,
+              'toTokenChainId': resObj.desChain, 
+              'partnerId': "0",
+              
+            }
+            
+             const quote=await getQuote(params)
+              setQuoteData(quote)
+              alert(quote.allowanceTo)
+              console.log(quote)
+
+              //Checking and Setting Allowance
+              if(window.ethereum) {
+                console.log('detected');
+            
+                try {
+                const accounts = await window.ethereum.request({
+                  method: "eth_requestAccounts",
+                });
+          
+                console.log(accounts[0])
+                const provider = new ethers.providers.Web3Provider(window.ethereum);
+                const signer = provider.getSigner();
+          
+                
+          
+                await checkAndSetAllowance(
+                  signer,
+                 resObj.sourceToken, 
+                  quote.allowanceTo, 
+                  ethers.constants.MaxUint256 
+                );
+                
+                //Executing the Transaction
+                const txResponse = await getTransaction({
+                  'fromTokenAddress': resObj.sourceToken,
+                  'toTokenAddress': resObj.desToken,
+                  'fromTokenChainId': resObj.sourceChain,
+                  'toTokenChainId': resObj.desChain, 
+                  'widgetId': 0, 
+                }, quote); 
+
+                const tx = await signer.sendTransaction(txResponse.txn)
+                try {
+                  await tx.wait();
+                  console.log(`Transaction mined successfully: ${tx.hash}`)
+                  alert(`Transaction mined successfully: ${tx.hash}`)
+                  
+                }
+                catch (error) {
+                  console.log(`Transaction failed with error: ${error}`)
+                }
+               
+                }
+                catch(err) {
+                console.log(err)
+                }
+              }
+          }
+
+        
+       
+      }}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
